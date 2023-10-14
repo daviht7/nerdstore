af689f303d63995cf88a890610d65af57b200527
davi holanda <daviht7@gmail.com>
2023-10-14 19:42:10 -0300

    Serviços de dominio

---
+++ NerdStore.Catalogo.Domain/EstoqueService.cs
@@ -0,0 +1,47 @@
+﻿namespace NerdStore.Catalogo.Domain
+{
+    public class EstoqueService : IEstoqueService
+    {
+        private readonly IProdutoRepository _produtoRepository;
+        //private readonly IMediatorHandler _mediator;
+
+        public EstoqueService(IProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+            //_mediator = mediator;
+        }
+
+        public async Task<bool> DebitarEstoque(Guid produtoId, int quantidade)
+        {
+            var produto = await _produtoRepository.ObterPorId(produtoId);
+
+            if (produto == null) return false;
+
+            if (!produto.PossuiEstoque(quantidade)) return false;
+
+            produto.DebitarEstoque(quantidade);
+
+            _produtoRepository.Atualizar(produto);
+
+            return await _produtoRepository.UnitOfWork.Commit();
+        }
+
+        public async Task<bool> ReporEstoque(Guid produtoId, int quantidade)
+        {
+            var produto = await _produtoRepository.ObterPorId(produtoId);
+
+            if (produto == null) return false;
+
+            produto.ReporEstoque(quantidade);
+
+            _produtoRepository.Atualizar(produto);
+
+            return await _produtoRepository.UnitOfWork.Commit();
+        }
+
+        public void Dispose()
+        {
+            _produtoRepository.Dispose();
+        }
+    }
+}
